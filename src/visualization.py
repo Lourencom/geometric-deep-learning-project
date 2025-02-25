77986ce205dab3ce1d5f8c0cb9b722ded63a44cd
Lourenco-dev-gpu-acs <lcm76@cam.ac.uk.com>
2025-02-25 20:15:01 +0000

    visualize matrices modular

--- src/visualization.py
+++ src/visualization.py
@@ -1,24 +1,12 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_attention_matrices(attention_matrices, aggregation="head", savepath=None):
-    assert aggregation in ["head", "layer"], "Must aggregate across 'head's or 'layer's"
-    
-    if aggregation == "head":
+def plot_attention_matrix(attention_matrix, savepath=None):
     # Visualize each layer's attention matrix (averaged across heads) using Seaborn
-        for layer_idx, attn_tensor in enumerate(attention_matrices):
-            # Remove the batch dimension (assuming batch_size = 1)
-            attn = attn_tensor[0]  # Shape: (num_heads, seq_len, seq_len)
-            
-            # Average across heads to get a single matrix per layer
-            attn_avg = attn.mean(axis=0)  # Shape: (seq_len, seq_len)
-            plt.figure(figsize=(8, 6))
-            sns.heatmap(attn_avg, cmap="Reds")
-            plt.title(f"Layer {layer_idx} (averaged over heads)")
-            plt.xlabel("Key Tokens")
-            plt.ylabel("Query Tokens")
-            plt.show()
-            if savepath:
-                plt.savefig(savepath)
-    else:
-        raise ValueError(f"Not yet implemented")
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(attention_matrix, cmap="Reds")
+    plt.xlabel("Key Tokens")
+    plt.ylabel("Query Tokens")
+    plt.show()
+    if savepath:
+        plt.savefig(savepath)
