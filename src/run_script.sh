381b1adab8e4b300281af0434d7eb7e3c66ce19f
Lourenco-dev-gpu-acs <lcm76@cam.ac.uk.com>
2025-03-14 14:29:58 +0000

    script used to run all

---
+++ src/run_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "First run: Qwen..."
+python main.py --prompt_ids 1 2 4 --output_dir qwen_comparisons_2/ --models "qwen,1.5b,instruct" "qwen,3b,instruct" "qwen,7b,instruct"
+
+echo "Second run: Gemma..."
+python main.py --prompt_ids 1 2 4 --output_dir gemma_comparisons_2/ --models "gemma,2b,instruct" "gemma,9b,instruct" "gemma,27b,instruct"
+
+echo "Third run: LLama..."
+python main.py --prompt_ids 1 2 4 --output_dir llama_comparisons_2/ --models "llama,1b,instruct" "llama,8b,instruct"
+
+echo "Fourth run: Mistral..."
+python main.py --prompt_ids 1 2 4 --output_dir mistral_comparisons_2/ --models "mistral,8b,instruct" "mistral,24b,instruct"
+
+
+echo "Compare all models..."
+python main.py --prompt_ids 1 2 4 --output_dir all_comparisons_2/ --models "mistral,8b,instruct" "mistral,24b,instruct" "qwen,1.5b,instruct" "qwen,3b,instruct" "qwen,7b,instruct" "llama,1b,instruct" "llama,8b,instruct" "gemma,2b,instruct" "gemma,9b,instruct" "gemma,27b,instruct"
+
+echo "All done!"
